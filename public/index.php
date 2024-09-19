27e461b895fada43ee31d468f13dec130a605d30
Vasquez11494 <cvasquez857@gmail.com>
2024-09-18 20:23:42 -0600

    conexion con FTPS local

--- public/index.php
+++ public/index.php
@@ -5,6 +5,7 @@ require_once __DIR__ . '/../includes/app.php';
 use MVC\Router;
 use Controllers\AppController;
 use Controllers\EmailController;
+use Controllers\FtpController;
 use Controllers\PdfController;
 
 $router = new Router();
@@ -13,6 +14,8 @@ $router->setBaseURL('/' . $_ENV['APP_NAME']);
 $router->get('/', [AppController::class,'index']);
 $router->get('/enviarformulario', [PdfController::class,'pdf']);
 $router->post('/email', [EmailController::class,'email']);
+$router->post('/email', [EmailController::class,'email']);
+$router->get('/conexion', [FtpController::class,'conexion']);
 
 // Comprueba y valida las rutas, que existan y les asigna las funciones del Controlador
 $router->comprobarRutas();
